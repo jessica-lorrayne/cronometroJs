44c5e048a95ff1be3a4d10008753e8500876f519
jessica <jessi.niza3@gmail.com>
2024-03-16 23:14:55 -0400

    ..

--- main.js
+++ main.js
@@ -1,7 +1,19 @@
-const minutos = document.getElementById('minutes')
-const segundos = document.getElementById('seconds')
-const milisegundos = document.getElementById('milleseconds')
+const minutesEl = document.getElementById('minutes')
+const secondsEl = document.getElementById('seconds')
+const millisecondsEl = document.getElementById('milleseconds')
 const btnStart = document.getElementById('startBtn')
 const btnReset = document.getElementById('resetBtn')
 const btnContinue = document.getElementById('continueBtn')
 const btnPause = document.getElementById('pauseBtn')
+
+
+let minutes = 0;
+let seconds = 0;
+let milleseconds = 0;
+let paused = false
+
+btnStart.addEventListener('click',startTime)
+
+function startTime() {
+
+}
